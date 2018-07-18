f37140c04b8a2b0e1d6e315fa1bbb07113633110
Nikita Gryzlov <nixel2007@gmail.com>
2018-07-18 19:11:04 +0300

    Перенос грамматики из репозитория dmpas/idea-onescript

--- src/main/java/org/bslplugin/BSLSyntaxHighlighter.java
+++ src/main/java/org/bslplugin/BSLSyntaxHighlighter.java
@@ -1,7 +1,8 @@
 package org.bslplugin;
 
 import com.intellij.lexer.Lexer;
-import com.intellij.openapi.editor.*;
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
+import com.intellij.openapi.editor.HighlighterColors;
 import com.intellij.openapi.editor.colors.TextAttributesKey;
 import com.intellij.openapi.fileTypes.SyntaxHighlighterBase;
 import com.intellij.psi.TokenType;
@@ -9,26 +10,42 @@ import com.intellij.psi.tree.IElementType;
 import org.bslplugin.psi.BSLTypes;
 import org.jetbrains.annotations.NotNull;
 
-import static com.intellij.openapi.editor.colors.TextAttributesKey.createTextAttributesKey;
-
 public class BSLSyntaxHighlighter extends SyntaxHighlighterBase {
-  public static final TextAttributesKey SEPARATOR =
-    createTextAttributesKey("BSL_SEPARATOR", DefaultLanguageHighlighterColors.OPERATION_SIGN);
-  public static final TextAttributesKey KEY =
-    createTextAttributesKey("BSL_KEY", DefaultLanguageHighlighterColors.KEYWORD);
-  public static final TextAttributesKey VALUE =
-    createTextAttributesKey("BSL_VALUE", DefaultLanguageHighlighterColors.STRING);
   public static final TextAttributesKey COMMENT =
-    createTextAttributesKey("BSL_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT);
+          TextAttributesKey.createTextAttributesKey("ONESCRIPT_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT);
   public static final TextAttributesKey BAD_CHARACTER =
-    createTextAttributesKey("BSL_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER);
+          TextAttributesKey.createTextAttributesKey("ONESCRIPT_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER);
+  public static final TextAttributesKey KEYWORDS =
+          TextAttributesKey.createTextAttributesKey("ONESCRIPT_KEYWORD", DefaultLanguageHighlighterColors.KEYWORD);
 
-  private static final TextAttributesKey[] BAD_CHAR_KEYS = new TextAttributesKey[]{BAD_CHARACTER};
-  private static final TextAttributesKey[] SEPARATOR_KEYS = new TextAttributesKey[]{SEPARATOR};
-  private static final TextAttributesKey[] KEY_KEYS = new TextAttributesKey[]{KEY};
-  private static final TextAttributesKey[] VALUE_KEYS = new TextAttributesKey[]{VALUE};
+  public static final TextAttributesKey STRING =
+          TextAttributesKey.createTextAttributesKey("ONESCRIPT_STRING", DefaultLanguageHighlighterColors.STRING);
+
+  public static final TextAttributesKey DATETIME =
+          TextAttributesKey.createTextAttributesKey("ONESCRIPT_DATETIME", DefaultLanguageHighlighterColors.STRING);
+
+  public static final TextAttributesKey LITERAL_CONSTANT =
+          TextAttributesKey.createTextAttributesKey("ONESCRIPT_LITERAL_CONSTANT", DefaultLanguageHighlighterColors.CONSTANT);
+
+  public static final TextAttributesKey COMPILER_DIRECTIVE =
+          TextAttributesKey.createTextAttributesKey("ONESCRIPT_COMPILER_DIRECTIVE", DefaultLanguageHighlighterColors.KEYWORD);
+
+  public static final TextAttributesKey USING_DIRECTIVE =
+          TextAttributesKey.createTextAttributesKey("ONESCRIPT_USING_DIRECTIVE", DefaultLanguageHighlighterColors.KEYWORD);
+
+  public static final TextAttributesKey PREPROCESSOR_DIRECTIVE =
+          TextAttributesKey.createTextAttributesKey("ONESCRIPT_PREPROCESSOR_DIRECTIVE", DefaultLanguageHighlighterColors.KEYWORD);
+
   private static final TextAttributesKey[] COMMENT_KEYS = new TextAttributesKey[]{COMMENT};
-  private static final TextAttributesKey[] EMPTY_KEYS = new TextAttributesKey[0];
+  private static final TextAttributesKey[] BAD_CHAR_KEYS = new TextAttributesKey[]{BAD_CHARACTER};
+  private static final TextAttributesKey[] KEYWORD_KEYS = new TextAttributesKey[]{KEYWORDS};
+  private static final TextAttributesKey[] STRING_KEYS = new TextAttributesKey[]{STRING};
+  private static final TextAttributesKey[] DATETIME_KEYS = new TextAttributesKey[]{DATETIME};
+  private static final TextAttributesKey[] LITERAL_CONSTANT_KEYS = new TextAttributesKey[]{LITERAL_CONSTANT};
+  private static final TextAttributesKey[] COMPILER_DIRECTIVE_KEYS = new TextAttributesKey[]{COMPILER_DIRECTIVE};
+  private static final TextAttributesKey[] USING_DIRECTIVE_KEYS = new TextAttributesKey[]{USING_DIRECTIVE};
+  private static final TextAttributesKey[] PREPROCESSOR_DIRECTIVE_KEYS = new TextAttributesKey[]{PREPROCESSOR_DIRECTIVE};
+
 
   @NotNull
   @Override
@@ -39,18 +56,32 @@ public class BSLSyntaxHighlighter extends SyntaxHighlighterBase {
   @NotNull
   @Override
   public TextAttributesKey[] getTokenHighlights(IElementType tokenType) {
-    if (tokenType.equals(BSLTypes.SEPARATOR)) {
-      return SEPARATOR_KEYS;
-    } else if (tokenType.equals(BSLTypes.KEY)) {
-      return KEY_KEYS;
-    } else if (tokenType.equals(BSLTypes.VALUE)) {
-      return VALUE_KEYS;
-    } else if (tokenType.equals(BSLTypes.COMMENT)) {
+    if (tokenType.equals(BSLTypes.COMMENT)) {
       return COMMENT_KEYS;
     } else if (tokenType.equals(TokenType.BAD_CHARACTER)) {
       return BAD_CHAR_KEYS;
+    } else if (tokenType.equals(BSLTypes.STRING)
+            || tokenType.equals(BSLTypes.STRINGSTART)
+            || tokenType.equals(BSLTypes.STRINGPART)
+            || tokenType.equals(BSLTypes.STRINGTAIL)) {
+      return STRING_KEYS;
+    } else if (tokenType.equals(BSLTypes.DATETIME)) {
+      return DATETIME_KEYS;
+    } else if (tokenType.equals(BSLTypes.COMPILER_DIRECTIVE)) {
+      return COMPILER_DIRECTIVE_KEYS;
+    } else if (tokenType.equals(BSLTypes.USING)) {
+      return USING_DIRECTIVE_KEYS;
+    } else if (tokenType.equals(BSLTypes.PREPROCESSOR)) {
+      return PREPROCESSOR_DIRECTIVE_KEYS;
+    } else if (tokenType.equals(BSLTypes.BOOLEAN_TRUE)
+            || tokenType.equals(BSLTypes.BOOLEAN_FALSE)
+            || tokenType.equals(BSLTypes.UNDEFINED)
+            || tokenType.equals(BSLTypes.NULL)) {
+      return LITERAL_CONSTANT_KEYS;
+    } else if (tokenType.toString().endsWith("_KEYWORD")) {
+      return KEYWORD_KEYS;
     } else {
-      return EMPTY_KEYS;
+      return new TextAttributesKey[0];
     }
   }
 }
--- src/main/resources/org/bslplugin/BSL.flex
+++ src/main/resources/org/bslplugin/BSL.flex
@@ -2,7 +2,7 @@ package org.bslplugin;
 
 import com.intellij.lexer.FlexLexer;
 import com.intellij.psi.tree.IElementType;
-import org.bslplugin.psi.BSLTypes;
+import static org.bslplugin.psi.BSLTypes.*;
 import com.intellij.psi.TokenType;
 
 %%
@@ -17,28 +17,117 @@ import com.intellij.psi.TokenType;
 
 CRLF=\R
 WHITE_SPACE=[\ \n\t\f]
-FIRST_VALUE_CHARACTER=[^ \n\f\\] | "\\"{CRLF} | "\\".
-VALUE_CHARACTER=[^\n\f\\] | "\\"{CRLF} | "\\".
-END_OF_LINE_COMMENT=("//")[^\r\n]*
-SEPARATOR=[=]
-KEY_CHARACTER=[^=\ \n\t\f\\] | "\\ "
+LINE_COMMENT=("//")[^\r\n]*
+LETTER=[:letter:] | "_"
+DIGIT=[:digit:]
+FLOAT_EXPONENT = [eE] [+-]? {DIGIT}+
+NUM_FLOAT = ( ( ({DIGIT}+ "." {DIGIT}*) | ({DIGIT}* "." {DIGIT}+) ) {FLOAT_EXPONENT}?) | ({DIGIT}+ {FLOAT_EXPONENT})
+IDENTIFIER = {LETTER} ( {LETTER} | {DIGIT} )*
+SUB_WORD="процедура"|"функция"|"procedure"|"function"
+ENDSUB_KEYWORD="endprocedure"|"endfunction"|"конецпроцедуры"|"конецфункции"
+EXPORT_KEYWORD="экспорт"|"export"
+BYVAL_KEYWORD="знач"|"val"
+TRUE="true"|"истина"
+FALSE="false"|"ложь"
+UNDEFINED="undefined"|"неопределено"
+NULL="null"
+QUOTE="\""
+SQUOTE="'"
+BAR="|"
+STRINGSTART= {QUOTE}([^\"\n\r])*
+STRING= {QUOTE}([^\"\n\r])*{QUOTE}
+STRINGTAIL= {BAR}([^\"\n\r])*{QUOTE}
+STRINGPART= {BAR}([^\"\n\r])*
+DATETIME= {SQUOTE}([^\'\n\r])*{SQUOTE}?
 
-%state WAITING_VALUE
+%state WAIT_IDENTIFIER
 
 %%
 
-<YYINITIAL> {END_OF_LINE_COMMENT}                           { yybegin(YYINITIAL); return BSLTypes.COMMENT; }
+<YYINITIAL> {
+{LINE_COMMENT}                            { return COMMENT; }
+"."                                       { yybegin(WAIT_IDENTIFIER); return DOT; }
 
-<YYINITIAL> {KEY_CHARACTER}+                                { yybegin(YYINITIAL); return BSLTypes.KEY; }
+"["                                       { return LBRACK; }
+"]"                                       { return RBRACK; }
 
-<YYINITIAL> {SEPARATOR}                                     { yybegin(WAITING_VALUE); return BSLTypes.SEPARATOR; }
+"("                                       { return LPAREN; }
+")"                                       { return RPAREN; }
 
-<WAITING_VALUE> {CRLF}({CRLF}|{WHITE_SPACE})+               { yybegin(YYINITIAL); return TokenType.WHITE_SPACE; }
+":"                                       { return COLON; }
+";"                                       { return SEMICOLON; }
+","                                       { return COMMA; }
 
-<WAITING_VALUE> {WHITE_SPACE}+                              { yybegin(WAITING_VALUE); return TokenType.WHITE_SPACE; }
+"="                                       { return ASSIGN; }
 
-<WAITING_VALUE> {FIRST_VALUE_CHARACTER}{VALUE_CHARACTER}*   { yybegin(YYINITIAL); return BSLTypes.VALUE; }
+"+"                                       { return PLUS; }
 
-({CRLF}|{WHITE_SPACE})+                                     { yybegin(YYINITIAL); return TokenType.WHITE_SPACE; }
+"-"                                       { return MINUS; }
 
-.                                                           { return TokenType.BAD_CHARACTER; }
+"<="                                      { return LESS_OR_EQUAL; }
+"<>"                                      { return NOT_EQUAL; }
+"<"                                       { return LESS; }
+
+"*"                                       { return MUL; }
+
+"/"                                       { return QUOTIENT; }
+
+"%"                                       { return MODULO; }
+
+">="                                      { return GREATER_OR_EQUAL; }
+">"                                       { return GREATER; }
+
+"?"                                       { return QUESTION; }
+
+{WHITE_SPACE} { return TokenType.WHITE_SPACE;}
+{TRUE} { return BOOLEAN_TRUE; }
+{FALSE} { return BOOLEAN_FALSE; }
+{UNDEFINED} { return UNDEFINED; }
+{NULL} { return NULL; }
+{STRINGSTART} { return STRINGSTART; }
+{STRING} { return STRING; }
+{STRINGTAIL} { return STRINGTAIL; }
+{STRINGPART} { return STRINGPART; }
+{DATETIME} { return DATETIME; }
+{SUB_WORD} { return SUB_KEYWORD; }
+{EXPORT_KEYWORD} { return EXPORT_KEYWORD; }
+{BYVAL_KEYWORD} { return BYVAL_KEYWORD; }
+{ENDSUB_KEYWORD} { return ENDSUB_KEYWORD; }
+"конецесли"|"endif" { return ENDIF_KEYWORD;}
+"конеццикла"|"enddo" { return ENDDO_KEYWORD; }
+"если"|"if" { return IF_KEYWORD; }
+"иначеесли"|"elsif" { return ELSEIF_KEYWORD; }
+"иначе"|"else" { return ELSE_KEYWORD; }
+"тогда"|"then" { return THEN_KEYWORD; }
+"пока"|"while" { return WHILE_KEYWORD; }
+"цикл"|"do" { return DO_KEYWORD; }
+"для"|"for" { return FOR_KEYWORD; }
+"по"|"to" { return TO_KEYWORD; }
+"каждого"|"each" { return EACH_KEYWORD; }
+"из"|"from" { return FROM_KEYWORD; }
+"попытка"|"try" { return TRY_KEYWORD; }
+"исключение"|"except" { return EXCEPT_KEYWORD; }
+"конецпопытки"|"endtry" { return ENDTRY_KEYWORD; }
+"возврат"|"return" { return RETURN_KEYWORD; }
+"продолжить"|"continue" { return CONTINUE_KEYWORD; }
+"вызватьисключение"|"raise" { return RAISE_KEYWORD; }
+"перем"|"var" { return VAR_KEYWORD; }
+"не"|"not" { return NOT_KEYWORD; }
+"или"|"or" { return OR_KEYWORD; }
+"и"|"and" { return AND_KEYWORD; }
+"новый"|"new" { return NEW_KEYWORD; }
+"&"{IDENTIFIER}? { return COMPILER_DIRECTIVE; }
+"#"("use"|"использовать")[^\r\n]* { return USING; }
+"#!"[^\r\n]* { return SHEBANG; }
+"#"[^\r\n]* { return PREPROCESSOR; }
+
+{IDENTIFIER}                              { return IDENTIFIER; }
+{NUM_FLOAT}                               { return FLOAT; }
+{DIGIT}+                                  { return DECIMAL; }
+}
+
+<WAIT_IDENTIFIER> {
+{IDENTIFIER}  { yybegin(YYINITIAL); return IDENTIFIER; }
+{WHITE_SPACE} { return TokenType.WHITE_SPACE; }
+.             { return TokenType.BAD_CHARACTER; }
+}

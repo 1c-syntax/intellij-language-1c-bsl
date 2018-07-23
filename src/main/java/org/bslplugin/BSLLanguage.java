ba0e40e928616ea78011f7c8d0d4fe226df6ee0d
Nikita Gryzlov <nixel2007@gmail.com>
2018-07-23 18:44:44 +0300

    Черновое подключение language-server

--- src/main/java/org/bslplugin/BSLLanguage.java
+++ src/main/java/org/bslplugin/BSLLanguage.java
@@ -1,6 +1,12 @@
 package org.bslplugin;
 
 import com.intellij.lang.Language;
+import org.bslplugin.lsp.server.BSLLanguageServer;
+import org.eclipse.lsp4j.jsonrpc.Launcher;
+import org.eclipse.lsp4j.launch.LSPLauncher;
+import org.eclipse.lsp4j.services.LanguageClient;
+import org.eclipse.lsp4j.services.LanguageClientAware;
+import org.eclipse.lsp4j.services.LanguageServer;
 
 public class BSLLanguage extends Language {
 
@@ -8,5 +14,18 @@ public class BSLLanguage extends Language {
 
   private BSLLanguage() {
     super("BSL");
+
+    LanguageServer server = new BSLLanguageServer();
+    Launcher<LanguageClient> launcher =
+            LSPLauncher.createServerLauncher(
+              server,
+              System.in,
+              System.out
+            );
+
+    LanguageClient client = launcher.getRemoteProxy();
+    ((LanguageClientAware)server).connect(client);
+
+    launcher.startListening();
   }
 }

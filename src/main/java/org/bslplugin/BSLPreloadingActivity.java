eca9828741c7519aa500191e24519412173cdb0c
Nikita Gryzlov <nixel2007@gmail.com>
2018-07-27 23:09:28 +0300

    Автостарт лэнг-сервера из зависимостей плагина

--- src/main/java/org/bslplugin/BSLPreloadingActivity.java
+++ src/main/java/org/bslplugin/BSLPreloadingActivity.java
@@ -1,17 +1,31 @@
 package org.bslplugin;
 
-import com.github.gtache.lsp.client.languageserver.serverdefinition.ArtifactLanguageServerDefinition;
+import com.github.gtache.lsp.client.languageserver.serverdefinition.ExeLanguageServerDefinition;
 import com.github.gtache.lsp.client.languageserver.serverdefinition.LanguageServerDefinition;
+import com.intellij.openapi.application.PathManager;
 import com.intellij.openapi.application.PreloadingActivity;
 import com.intellij.openapi.progress.ProgressIndicator;
 import org.jetbrains.annotations.NotNull;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BSLPreloadingActivity extends PreloadingActivity {
 
   @Override
   public void preload(@NotNull ProgressIndicator indicator) {
+
+    String pluginsPath = PathManager.getPluginsPath();
+    Path languageServer = Paths.get(pluginsPath, "Language 1C (BSL)", "lib", "languageserver-1.0.jar");
+
+    List<String> args = new ArrayList<>();
+    args.add("-jar");
+    args.add(languageServer.toString());
+    args.add("-debug");
     LanguageServerDefinition.register(
-      new ArtifactLanguageServerDefinition("bsl", "org.bslplugin:languageserver:1.0", "org.bslplugin.lsp.server.BSLLSPLauncher", new String[]{})
+      new ExeLanguageServerDefinition("bsl", "java", args.toArray(new String[0]))
     );
   }
 

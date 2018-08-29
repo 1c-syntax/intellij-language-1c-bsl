4721d9bf53696dc00dc0b0a720bac4908b4a6e61
Nikita Gryzlov <nixel2007@gmail.com>
2018-08-30 01:03:44 +0300

    Добавлен перехватчик jsonrpc запросов

--- languageserver/src/main/java/org/github/_1c_syntax/intellij/bsl/lsp/server/BSLLSPLauncher.java
+++ languageserver/src/main/java/org/github/_1c_syntax/intellij/bsl/lsp/server/BSLLSPLauncher.java
@@ -6,18 +6,18 @@ import org.eclipse.lsp4j.services.LanguageClient;
 import org.eclipse.lsp4j.services.LanguageClientAware;
 import org.eclipse.lsp4j.services.LanguageServer;
 
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 
 public class BSLLSPLauncher {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws FileNotFoundException {
 
     InputStream in = System.in;
     OutputStream out = System.out;
 
     LanguageServer server = new BSLLanguageServer();
-    Launcher<LanguageClient> launcher = LSPLauncher.createServerLauncher(server, in, out);
+    PrintWriter tracer = new PrintWriter(new File("D:\\git\\1c-syntax\\idea-language-1c-bsl\\intellij-bsl\\build\\idea-sandbox\\system\\log\\tracer.log"));
+    Launcher<LanguageClient> launcher = LSPLauncher.createServerLauncher(server, in, out, false, tracer);
 
     LanguageClient client = launcher.getRemoteProxy();
     ((LanguageClientAware) server).connect(client);
